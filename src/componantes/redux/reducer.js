1e1134c265a3e72d8281a193bc6ee90b6872b52e
Jo <jo@Jos-MacBook-Air.local>
2023-01-23 23:19:20 +0300

    Fix redux states

--- src/componantes/redux/reducer.js
+++ src/componantes/redux/reducer.js
@@ -1,26 +1,31 @@
 import ActionType from './actionType';
 
+const allCoin = [];
+
 export default function CoinsReducer(state = [], { type, payload }) {
   switch (type) {
     case ActionType.DETAILS:
     {
       const result = [];
-      const newArray = state.find((item) => item.id === payload);
+      const newArray = allCoin.slice(0, 100).find((item) => item.id === payload);
       result.push(newArray);
       return result;
     }
     case ActionType.FILTERBYRANK:
     {
-      const newArray = state.filter((item) => item.rank <= payload);
+      const newArray = allCoin.slice(0, 100).filter((item) => item.rank <= payload);
       return newArray;
     }
     case ActionType.SEARCH:
     {
-      const newArray = state.filter((item) => item.name.toLowerCase().includes(payload.toLowerCase()));
+      const newArray = allCoin.splice(0, 100).filter((item) => item.name.toLowerCase().includes(payload.toLowerCase()));
       return newArray;
     }
     case ActionType.FETCHDATA:
-      return payload;
+    {
+      allCoin.push(...payload);
+      return allCoin.slice(0, 100);
+    }
     default:
       return state;
   }
