18d091fda117ac05c4f89fd50f29ff20e891117a
Jo <jo@Jos-MacBook-Air.local>
2023-01-31 17:51:07 +0300

    Fix logo link

--- src/componantes/Nav/Nav.js
+++ src/componantes/Nav/Nav.js
@@ -1,20 +1,27 @@
 import './Nav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMicrophone, faGear, faBitcoinSign } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import SearchInput from '../coins/searchInput';
+import { setData } from '../redux/actions';
 
-const Nav = () => (
-  <header className="header">
-    <Link className="logo" to="/">
-      <FontAwesomeIcon className="logoImg" icon={faBitcoinSign} />
-    </Link>
-    <div className="feature">
-      <SearchInput />
-      <FontAwesomeIcon icon={faMicrophone} />
-      <FontAwesomeIcon icon={faGear} />
-    </div>
-  </header>
-);
+const Nav = () => {
+  const dispatch = useDispatch();
+  const logoHandler = () => {
+    dispatch(setData());
+  };
+  return (
+    <header className="header">
+      <div className="logo">
+        <FontAwesomeIcon className="logoImg" icon={faBitcoinSign} onClick={logoHandler} />
+      </div>
+      <div className="feature">
+        <SearchInput />
+        <FontAwesomeIcon icon={faMicrophone} />
+        <FontAwesomeIcon icon={faGear} />
+      </div>
+    </header>
+  );
+};
 
 export default Nav;
